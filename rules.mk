c937c1d77878de3c3ac460e5b76b14d214761b33
Fang Jin <windmaomao@gmail.com>
2023-09-09 10:24:27 -0400

    add config and rules

--- rules.mk
+++ rules.mk
@@ -1,1 +1,14 @@
 SRC += source.c
+
+MOUSEKEY_ENABLE = yes   
+AUTO_SHIFT_ENABLE = yes 
+CAPS_WORD_ENABLE = no   
+SWAP_HANDS_ENABLE = no  
+RGBLIGHT_ENABLE = yes   
+RGB_MATRIX_ENABLE = no
+OLED_ENABLE     = no
+OLED_DRIVER     = SSD1306
+VIA_ENABLE      = yes   
+LTO_ENABLE      = yes
+ENCODER_ENABLE  = yes   
+ENCODER_MAP_ENABLE = no
